cab3493ee66e2cbf76bd54820f544ea16dafd283
harshkan <harshkansara888@gmail.com>
2018-06-20 22:29:44 +0530

    helloo

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { Ng2SmartTableModule } from 'ng2-smart-table';
+import { HttpClientModule } from '@angular/common/http';
+import { TableService } from './table/table.service';
+
+import { AppComponent } from './app.component';
+import { TableComponent } from './table/table.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TableComponent
+  ],
+  imports: [
+    BrowserModule,
+    Ng2SmartTableModule,
+    HttpClientModule
+  ],
+  providers: [TableService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
