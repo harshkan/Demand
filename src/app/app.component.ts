e06f7d43c706893c4560d940b697b52e8567b101
HARSH KANSARA <harshkansara888@gmail.com>
2018-06-25 22:52:12 +0530

    ssffsssssg

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,79 @@
-import { Component } from '@angular/core';
+import { Component , OnInit} from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
+import { DomSanitizer, SafeHtml } from "@angular/platform-browser";
+import 'rxjs/add/observable/of';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'app';
+   selected: string;
+   data :any=[];
+   continents = [{
+      id: 1,
+      name: 'Asia',
+      population: '4,157,300,000'
+    }, {
+      id: 2,
+      name: 'Africa',
+      population: '1,030,400,000'
+    }, {
+      id: 3,
+      name: 'Europe',
+      population: '738,600, 000'
+    }, {
+      id: 4,
+      name: 'North America',
+      population: '461,114,000'
+    }, {
+      id: 5,
+      name: 'South America',
+      population: '390,700,000'
+    }, {
+      id: 6,
+      name: 'Australia',
+      population: '36,700,000'
+    }, {
+      id: 7,
+      name: 'Antartica',
+      population: 0
+    }
+    ];
+    autocompleListFormatter = (data: any) : SafeHtml => {
+       let html = `<span>${data.name}</span>`;
+       return this._sanitizer.bypassSecurityTrustHtml(html);
+     }
+  constructor(public http: Http,private _sanitizer: DomSanitizer) {
+
+
+  }
+    ngOnInit() {
+
+        this.getdata();
+
+    }
+getdata()
+{
+  let url: string = './assets/data.json'
+  this.http.get(url)
+    .map(res => {
+      let json = res.json();
+      console.log(json);
+      this.data=json;
+      return json;
+    })
+  
+}
+  observableSource = (keyword: any): Observable<any[]> => {
+
+
+
+      return Observable.of(this.data);
+
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { Ng2SmartTableModule } from 'ng2-smart-table';
-
+import { Ng2AutoCompleteModule } from 'ng2-auto-complete';
 import { AppComponent } from './app.component';
 import { TableComponent } from './table/table.component';
-
+import { HttpClientModule } from '@angular/common/http';
+import { AdminComponent } from './admin/admin.component';
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
-    TableComponent
+    TableComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
     Ng2SmartTableModule,
+    Ng2AutoCompleteModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
